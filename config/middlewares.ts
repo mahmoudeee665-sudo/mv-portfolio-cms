ed3671556cf709683deed7d57f017bef1fe51951
mahmoudeee665-sudo <mahmoudeee665@gmail.com>
2025-09-28 16:38:03 +0400

    chore: add CORS config for Vercel frontend

--- config/middlewares.ts
+++ config/middlewares.ts
@@ -1,9 +1,19 @@
 export default [
-  'strapi::logger',
   'strapi::errors',
+  {
+    name: 'strapi::cors',
+    config: {
+      origin: [
+        'http://localhost:3000',
+        'https://mv-portfolio-ev9iclyai-mahmouds-projects-7910caab.vercel.app/', // your Vercel domain
+      ],
+      methods: ['GET','POST','PUT','PATCH','DELETE','OPTIONS'],
+      headers: '*',
+      keepHeadersOnError: true,
+    },
+  },
   'strapi::security',
-  'strapi::cors',
-  'strapi::poweredBy',
+  'strapi::logger',
   'strapi::query',
   'strapi::body',
   'strapi::session',
